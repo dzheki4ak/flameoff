ed27ea575b9566a8547ccb2b1802e784da975976
Ihor Kychak <Igor.ki4@gmail.com>
2023-11-10 15:15:34 -0500

    case studdies updated

--- constants/studiesCardDetails.js
+++ constants/studiesCardDetails.js
@@ -1,25 +1,25 @@
-import flameBuilding from '@/public/images/case_images/flame_building.svg';
-import painting from '@/public/images/case_images/painting.svg';
-import pyramid from '@/public/images/case_images/cube_pyramid.svg';
+import Stadium from '@/public/images/case_images/stadium.svg';
+import Beams from '@/public/images/case_images/steel_beams.svg';
+import Ceiling from '@/public/images/case_images/wood_ceiling.svg';
 
 const studiesCardDetails = [
   {
-    imgSrc: flameBuilding,
-    title: 'Title',
-    text: 'Egestas elit dui scelerisque ut eu purus aliquam vitae habitasse.',
-    altTxt: 'building on fire',
+    imgSrc: Stadium,
+    title: 'World cup 2022 2 hour fire rating on DRYWALL',
+    text: 'In 2022 FlameOFF supplied the Ras Abu Aboud Stadium with intumescent paint for the FIFA 2022 World Cup....',
+    altTxt: 'stadium seats',
   },
   {
-    imgSrc: painting,
-    title: 'Title',
-    text: 'Id eros pellentesque facilisi id mollis faucibus commodo enim.',
-    altTxt: 'painting process',
+    imgSrc: Beams,
+    title: '2 Hour fire protection on STEEL BEAMS',
+    text: 'A building on Madison Ave in New York City required a 2 hour fire rating on steel beams. FlameOFF coatings supplied...',
+    altTxt: 'steel beams',
   },
   {
-    imgSrc: pyramid,
-    title: 'Title',
-    text: 'Nunc, pellentesque velit malesuada non massa arcu.',
-    altTxt: 'cubes pyramid',
+    imgSrc: Ceiling,
+    title: '1 Hour fire rating on WOOD CEILING ASSEBLY',
+    text: 'The Bramble Theater in a historic district of Chicago’s Anderson Village was due for a renovation. Fire Barrier intumescent...',
+    altTxt: 'Wood ceiling',
   },
 ];
 
