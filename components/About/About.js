5d2a150e6dd79df4383c015c4991cbed4784fa6a
Ihor Kychak <Igor.ki4@gmail.com>
2023-12-20 16:52:48 -0500

    Section margins layouts

--- components/About/About.js
+++ components/About/About.js
@@ -1,7 +1,9 @@
+'use client';
 import Image from 'next/image';
 import SectionHeading from '../SectionHeading/SectionHeading';
 import { aboutDet } from '@/constants/sectionHeadingDetails';
-import about from '@/public/images/about_us_image.svg';
+import AboutImg from '@/public/images/about_us_image.svg';
+import AboutMob from '@/public/images/about_us_image_mob.svg'
 
 import './About.scss';
 
@@ -15,11 +17,16 @@ const About = () => {
           fire safety solutions that will improve the fire rating in
           your construction projects.
         </p>
-        <Image
-          className="about__details_image"
-          src={about}
-          alt="firefighters image"
-        />
+        <picture>
+          <source media="(min-width: 730px)" srcSet="../../public/images/about_us_image.svg" />
+          <source media="(min-width: 100px)" srcSet="../../public/images/about_us_image_mob.svg" />
+          <source srcSet="../../public/images/about_us_image_mob.svg" />
+          <Image
+            className="about__details_image"
+            src={AboutImg}
+            alt="firefighters image"
+          />
+        </picture>
       </div>
     </section>
   );
