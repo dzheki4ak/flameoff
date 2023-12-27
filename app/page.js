6a2ba2eb9f244ff0e3adaeedaff97438a1f06df8
Ihor Kychak <Igor.ki4@gmail.com>
2023-12-27 15:05:16 -0500

    primar carousel added

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 import Products from '@/components/Products/Products';
 import Benefits from '@/components/Benefits/Benefits';
 import About from '@/components/About/About';
-import './page.scss';
 import Studies from '@/components/Studies/Studies';
 import ContactUs from '@/components/Contact/ContactUs';
 import Industries from '@/components/Indastries/Industries';
+import './page.scss';
 
 export default function Home() {
   return (
